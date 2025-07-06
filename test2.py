9687f28056d933267df76358720201e80dee64e7
Suresh Baskaran <simbha66@gmail.com>
2025-07-06 20:11:11 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,23 @@
+
+import pyautogui
+import time
+#Mouse Recomendations 
+time.sleep(2)
+#pyautogui.leftClick (1799,8)
+#time.sleep(2)
+#pyautogui.doubleClick (1078,399)
+#pyautogui.scroll(-500)
+pyautogui.leftClick (423,290)
+
+#keyboard Operationsp
+time.sleep(2)
+pyautogui.typewrite("Social Eagle.ai")
+pyautogui.press('enter')
+
+#time.sleep(4)
+
+#pyautogui.hotkey('ctrl',"")
+
+#image
+#location = pyautogui.locateAllOnScreen('cp-image.jpg', confidence = 0.8) 
+#print(location)
