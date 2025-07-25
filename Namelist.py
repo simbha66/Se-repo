83f5309cb6e34fd3774ee2631321ba7df0f1a62e
Suresh Baskaran <simbha66@gmail.com>
2025-07-25 22:21:12 +0530

    Add files via upload

---
+++ Namelist.py
@@ -0,0 +1,6 @@
+# Store 5 names in a list
+names = ["Suresh", "NAvitha", "Simbha", "Bharath", "Taksh"]
+
+# Print each name with its length
+for name in names:
+    print(f"Name: {name}, Length: {len(name)}")
---
+++ test.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+import webbrowser
+
+webbrowser .open("https://www.chatgpt.com")
+time.sleep(5)
